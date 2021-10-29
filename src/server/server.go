ddd0319db84066b4e606c269ddf18619f5dcd415
Simon Justesen <simon.justesen@gmail.com>
2021-10-29 12:09:44 +0200

    Update

--- src/server/server.go
+++ src/server/server.go
@@ -3,9 +3,12 @@ package server
 
 import (
   "fmt"
+  "log"
+  "io"
+  "os"
   "net/http"
   "github.com/gin-gonic/gin"
-  "github.com/gin-contrib/sessions"
+  // "github.com/gin-contrib/sessions"
 )
 
 const (
@@ -27,7 +30,10 @@ func Bootup() {
     restricted := r.Group("/user")
     restricted.Use(AuthRequired) 
     {
-      restricted.GET("/upload", uploadfile)
+      restricted.GET("/upload", func(c *gin.Context) {
+          c.HTML(http.StatusOK, "upload.tmpl", gin.H{})
+      })
+      restricted.POST("/upload", uploadfile)
       restricted.GET("/me", profile)
     }
     
@@ -35,6 +41,19 @@ func Bootup() {
 }
 
 func uploadfile(c *gin.Context) {
+  file, header, err := c.Request.FormFile("file") 
+  filename := header.Filename
+  out, err := os.Create("public/" + filename)
+  defer out.Close()
+  
+  _, err = io.Copy(out, file)
+  
+  if err != nil {
+    log.Fatal(err)
+    c.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+    return
+  }
+
 }
 
 func profile(c *gin.Context) {
