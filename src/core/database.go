65f5c45e23dc70da7a43a6d33ef7c69799cb97c9
Simon Justesen <simon.justesen@gmail.com>
2021-11-07 12:26:57 +0100

    Update

--- src/core/database.go
+++ src/core/database.go
@@ -1,15 +1,28 @@
 package database
 
-// "gorm.io/driver/mysql"
-// "gorm.io/gorm"
+import (
+	"database/sql"
+	"time"
+
+	//"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 type DbConn struct {
 	username string
 	password string
 }
 
-func (db *Database) Setup() {
+type Project struct {
+	ID          uint `gorm:"primaryKey"`
+	Name        string
+	ActivatedAt sql.NullTime
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
 
+func (db *gorm.DB) Setup() {
+	db.AutoMigrate(&Project)
 }
 
 func down() {
