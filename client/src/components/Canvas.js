d06d3cf2a518848e0e6d119609ea6d77f8cc3f3d
Simon Justesen <simon.justesen@gmail.com>
2021-12-04 00:44:24 +0100

    Added rulers. Needs more work

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import './Canvas.css';
 import { fabric } from '../lib/fabric.min.js';
+import Guides from "@scena/guides";
 
 class Canvasa extends React.Component {
   
 
    componentDidMount() {
       this.initCanvas();
+      this.initGuides();
    }
 
    render() {
       return <div className="canvascontainer">
-         <div className="canvas"><canvas id="c"></canvas></div>
+         <div className="canvas"><div className="ruler" style={{width: 60 + 'px' }}></div>
+         <canvas id="c"></canvas></div>
       </div>
+   }
+
+   initGuides() {
+      const guides = new Guides(document.querySelector('.ruler'), {
+         type: "vertical",
+     }).on("changeGuides", e => {
+         console.log(e.guides);
+     });
+     
+     
+     let scrollX = 0;
+     let scrollY = 0;
+     window.addEventListener("resize", () => {
+         guides.resize();
+     });
+     
+     window.addEventListener("wheel", e => {
+         scrollX += e.deltaX;
+         scrollY += e.deltaY;
+     
+         guides.scrollGuides(scrollY);
+         guides.scroll(scrollX);
+     }); 
    }
 
    initCanvas() {
