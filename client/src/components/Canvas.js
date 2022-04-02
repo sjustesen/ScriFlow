6936b92cca0a09607b827a0cbb9fab7eda0c3beb
Simon Justesen <simon.justesen@gmail.com>
2022-04-02 20:20:13 +0200

    Update

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -3,34 +3,12 @@ import './Canvas.css';
 
 import { fabric } from '../lib/fabric.min.js';
 
-import Guides from "@scena/guides";
 import SFCanvasLib from '../core/drawing/canvaslib'
 
 class SFCanvas extends React.Component {
 
    componentDidMount() {
-      this.initCanvas();
-      this.initGuides();
-      this.registerCanvasEvents();
-
-      this.sfcanvas = new SFCanvasLib(this.canvas);
-
-   }
-
-   registerCanvasEvents() {
-      window.addEventListener('XmlDocumentLoaded', this.XMLDocumentLoaded)
-      window.addEventListener('LayersPanelChanged', () => {
-         console.log('Update Layer Panel signaled..')
-      })
-
-      window.addEventListener('CanvasRedrawn', (event) => {
-      });
-
-      window.addEventListener('CanvasUpdated', (event) => {
-         for (let canvas_object of event.detail) {
-            this.sfcanvas.drawObject(canvas_object);
-         }
-      });
+      this.sfcanvas = new SFCanvasLib();
 
    }
 
@@ -45,67 +23,6 @@ class SFCanvas extends React.Component {
             </div>
          </div>
       </div>
-   }
-
-   initGuides() {
-      const guide1 = new Guides(document.querySelector('.ruler'), {
-         type: "vertical",
-      }).on("changeGuides", e => {
-         console.log(e.guides);
-      });
-
-      const guide2 = new Guides(document.querySelector('.rulerHorizontal'), {
-         type: "horizontal",
-      }).on("changeGuides", e => {
-         console.log(e.guides);
-      });
-
-      let scrollX = 0;
-      let scrollY = 0;
-
-      window.addEventListener("resize", () => {
-         guide1.resize();
-         guide2.resize();
-      });
-
-      window.addEventListener("wheel", e => {
-         scrollX += e.deltaX;
-         scrollY += e.deltaY;
-
-         guide1.scrollGuides(scrollY);
-         guide1.scroll(scrollX);
-      });
-   }
-
-   initCanvas() {
-      let grandparent = document.querySelector('.canvascontainer');
-      let parent = document.querySelector('.canvas');
-
-      let width = grandparent.clientWidth - 60;
-
-      parent.setAttribute('style', 'width: ' + width + 'px');
-
-      this.canvas = new fabric.Canvas('c');
-      this.canvas.setHeight(parent.clientHeight);
-      this.canvas.setWidth(parent.clientWidth);
-
-      window.addEventListener('resize', (e) => {
-         let width = grandparent.clientWidth - 60;
-         parent.setAttribute('style', 'width: ' + width + 'px');
-         this.canvas.setHeight(parent.clientHeight);
-         this.canvas.setWidth(parent.clientWidth);
-
-      })
-   }
-
-   // event
-   XMLDocumentLoaded = (data) => {
-      console.log('xmldoc loaded');
-   }
-
-   // event
-   XMLDocumentUnloaded() {
-      console.log('xmldoc unloaded');
    }
 
 }
